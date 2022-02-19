4d025f24374dc3a5801b91ab5eb4568dc543729d
Kleryton <kleryton.dev@gmail.com>
2022-02-19 09:25:07 -0300

    initial commit

---
+++ src/test/java/com/api/CadastroUsuarios/CadastroUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.CadastroUsuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
